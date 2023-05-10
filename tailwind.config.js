ae2e8fc161d8ec369816f7d5808f70dd397b2652
Maria Lundqvist <mari28rd@stud.kea.dk>
2023-05-10 16:05:56 +0200

    added colours and font sizes to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,10 +9,52 @@ module.exports = {
       },
     },
     colors: {
-      //add colors here
+      blue: {
+        DEFAULT: "#1D00B2",
+        75: "#5640C5",
+        50: "#8E80D9",
+        25: "C6BFEC",
+      },
+      grey: {
+        DEFAULT: "#5F5F5D",
+        75: "#919190",
+        50: "#C9C9C9",
+        25: "#EAEAE9 ",
+      },
+      orange: {
+        DEFAULT: "#E36409",
+      },
+      link: {
+        DEFAULT: "#035FCC",
+      },
+      white: {
+        DEFAULT: "#FAFAFA",
+      },
+      error: {
+        DEFAULT: "#EE3900",
+      },
     },
     fontFamily: {
       //add font families here
+    },
+    fontSize: {
+      12: "0.75rem",
+      14: "0.875rem",
+      base: "1rem",
+      18: "1.125rem",
+      20: "1.25rem",
+      22: "1.375rem",
+      24: "1.5rem",
+      32: "2rem",
+      40: "2.5rem",
+      48: "3rem",
+      64: "4rem",
+    },
+    fontWeight: {
+      normal: 400,
+      medium: 500,
+      semibold: 600,
+      bold: 700,
     },
   },
   plugins: [],
